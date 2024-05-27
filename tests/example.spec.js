3231fd8bf7d9714a301eb5b4305b3c5956b21fe1
fgnogueira <77158631+fgnogueira@users.noreply.github.com>
2024-05-27 10:31:13 +0000

    new comments to the tests

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,7 +7,7 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
-
+// Adding some comments to the tests fo testing !!!
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
